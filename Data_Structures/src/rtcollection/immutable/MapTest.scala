e7f00c9710d7d5192a66d0d49f019e5f8e46e365
Lynna Cekova <lynna.cekova@gmail.com>
2016-02-13 20:09:59 -0500

    Put and get implemented completely. Iterable is next.

--- Data_Structures/src/rtcollection/immutable/MapTest.scala
+++ Data_Structures/src/rtcollection/immutable/MapTest.scala
@@ -3,6 +3,25 @@ package rtcollection.immutable
 /**
   * Created by rtorres on 2/9/2016.
   */
-class MapTest {
+object MapTest extends App {
+  val testMap = new HashMap[String, Int]
+
+  testMap += KVPair("Hello", 12)
+  testMap += KVPair("World", 22)
+  testMap += KVPair("Again", 32)
+  testMap += KVPair("Times", 42)
+  testMap += KVPair("The", 52)
+  testMap += KVPair("Best", 62)
+  testMap += KVPair("Sort", 72)
+  testMap += KVPair("Of", 82)
+  testMap += KVPair("Thing", 92)
+  testMap += KVPair("A", 112)
+  testMap += KVPair("Whole", 122)
+  testMap += KVPair("New", 132)
+  testMap += KVPair("Way", 142)
+  testMap += KVPair("To", 152)
+  testMap += KVPair("Screw", 162)
+  testMap += KVPair("No", 972)
+  testMap += KVPair("One", 982)
 
 }
